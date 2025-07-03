693ef64e2a8c2e6f05983f8283bfc1a4e8082935
17Gauri <mandharegauri21@gmail.com>
2025-07-03 16:37:51 +0530

    Restore: Re-added userModel.js with correct case

---
+++ server/models/userModel.js
@@ -0,0 +1,12 @@
+// server/models/userModel.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  creditBalance: { type: Number, default: 0 }
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
